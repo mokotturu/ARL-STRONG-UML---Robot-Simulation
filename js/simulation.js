3c82d8c95ffcc4c55110825c3afa4f5b5f1ba82d
Monish Reddy Kotturu <51919815+Monish-Reddy-Kotturu@users.noreply.github.com>
2020-10-01 20:08:47 -0400

    Added autonomous bot
    Autonomous bot, more cell properties, speed setting, explored cell color coding,
    and generalized bot functions

--- js/simulation.js
+++ js/simulation.js
@@ -1,37 +1,104 @@
 var canvas = document.getElementById("map");
 var context = canvas.getContext("2d");
 
+context.canvas.width = window.innerWidth/3;
+context.canvas.height = window.innerWidth/3;
+
 var rows = 30;
 var columns = 30;
 
-let canvasWidth = canvas.width;
-let canvasHeight = canvas.height;
-let boxWidth;
-let boxHeight;
+var canvasWidth = canvas.width;
+var canvasHeight = canvas.height;
+var boxWidth;
+var boxHeight;
 
-var cellColor = "black";
-var botColor = "blue";
+const CELL_COLOR = "black";
+const USER_BOT_COLOR = "blue";
+const AUTO_BOT_COLOR = "red";
+const EXPLORED_COLOR = "white";
 
 var map = [];
-let botIndex = 0;
+var bots = [];
+
+var count = 0;
+var waitCount = 4;
 
 createMap();
 drawMap();
-// to prevent spawning on top of a wall
-do {
-  botIndex = Math.floor(Math.random() * map.length);
-} while(map[botIndex].isWall);
-var bot = map[botIndex];
-spawnBot(bot.x, bot.y);
 
-// creates an array containing cells with x and y positions and wall details
+var userBot = {loc: getRandomLoc(), color: USER_BOT_COLOR, dir: 1};
+var autoBot = {loc: getRandomLoc(), color: AUTO_BOT_COLOR, dir: 1};
+bots.push(userBot, autoBot);
+
+spawnBot(userBot);
+spawnBot(autoBot);
+
+// auto bot movement
+function loop() {
+  requestAnimationFrame(loop);
+
+  // set speed
+  if (++count < waitCount) {
+    return;
+  }
+
+  count = 0;
+
+  switch (autoBot.dir) {
+    case 1:
+      if (Math.floor(map[autoBot.loc].y) != 1 && !map[autoBot.loc - columns].isWall && !map[autoBot.loc - columns].isOccupied) {
+        map[autoBot.loc].isOccupied = !map[autoBot.loc].isOccupied;
+        map[autoBot.loc].isExplored = true;
+        autoBot.loc -= columns;
+        refreshMap();
+      } else {
+        autoBot.dir = Math.random() < Math.random() ? 4 : 2;
+      }
+      break;
+    case 2:
+      if (Math.floor(map[autoBot.loc].x) != Math.floor(1 + (columns - 1) * (canvasWidth / columns)) && !map[autoBot.loc + 1].isWall && !map[autoBot.loc + 1].isOccupied) {
+        map[autoBot.loc].isOccupied = !map[autoBot.loc].isOccupied;
+        map[autoBot.loc].isExplored = true;
+        autoBot.loc++;
+        refreshMap();
+      } else {
+        autoBot.dir = Math.random() < Math.random() ? 1 : 3;
+      }
+      break;
+    case 3:
+      if (Math.floor(map[autoBot.loc].y) != Math.floor(1 + (rows - 1) * (canvasHeight / rows)) && !map[autoBot.loc + columns].isWall && !map[autoBot.loc + columns].isOccupied) {
+        map[autoBot.loc].isOccupied = !map[autoBot.loc].isOccupied;
+        map[autoBot.loc].isExplored = true;
+        autoBot.loc += columns;
+        refreshMap();
+      } else {
+        autoBot.dir = Math.random() < Math.random() ? 2 : 4;
+      }
+      break;
+    case 4:
+      if (Math.floor(map[autoBot.loc].x) != 1 && !map[autoBot.loc - 1].isWall && !map[autoBot.loc - 1].isOccupied) {
+        map[autoBot.loc].isOccupied = !map[autoBot.loc].isOccupied;
+        map[autoBot.loc].isExplored = true;
+        autoBot.loc--;
+        refreshMap();
+      } else {
+        autoBot.dir = Math.random() < Math.random() ? 3 : 1;
+      }
+      break;
+    default:
+      // nothing
+      break;
+  }
+}
+
+// creates an array containing cells with x and y positions and additional details
 function createMap() {
   map = [];
   boxWidth = canvasWidth/columns - 2;
   boxHeight = canvasHeight/rows - 2;
   for (let y = 1; y < canvasHeight; y += boxHeight + 2) {
     for (let x = 1; x < canvasWidth; x += boxWidth + 2) {
-      map.push({x: x, y: y, isWall: Math.random() < 0.1});  // 10% chance that a cell will be a wall
+      map.push({x: x, y: y, isWall: Math.random() < 0.1, isOccupied: false, isExplored: false});  // 10% chance that a cell will be a wall during initialization
     }
   }
   console.log(map);
@@ -39,24 +106,31 @@ function createMap() {
 
 // renders the map on the screen
 function drawMap() {
-  context.fillStyle = cellColor;
   map.forEach(item => {
-    if (!item.isWall) context.fillRect(item.x, item.y, boxWidth, boxHeight);
+    if (item.isExplored && !item.isWall) {
+      context.fillStyle = EXPLORED_COLOR;
+      context.fillRect(item.x, item.y, boxWidth, boxHeight);
+    } else if (!item.isExplored && !item.isWall) {
+      context.fillStyle = CELL_COLOR;
+      context.fillRect(item.x, item.y, boxWidth, boxHeight);
+    }
   });
 }
 
-// spawns the bot in the specified location
-function spawnBot(x, y) {
+// spawns the bot in its location
+function spawnBot(bot) {
   console.log(bot);
-  context.fillStyle = botColor;
-  context.fillRect(x, y, boxWidth, boxHeight);
+  context.fillStyle = bot.color;
+  context.fillRect(map[bot.loc].x, map[bot.loc].y, boxWidth, boxHeight);
+  map[bot.loc].isOccupied = true;
 }
 
-// redraws the map and spawns the bot in its new location
+// redraws the map and spawns the bots in their new location
 function refreshMap() {
   context.clearRect(0, 0, canvasWidth, canvasHeight);
   drawMap();
-  spawnBot(bot.x, bot.y);
+  spawnBot(userBot);
+  spawnBot(autoBot);
 }
 
 // takes the new grid size and modifies the map
@@ -67,11 +141,22 @@ function modifyMap() {
     alert("Incorrect input. Please enter a positive integer.");
     return;
   }
-  rows = parseInt(document.getElementById("rows").value);
-  columns = parseInt(document.getElementById("columns").value);
+  rows = parseInt(rowsInput);
+  columns = parseInt(columnsInput);
   createMap();
-  bot = map[Math.floor(Math.random() * map.length)];
+  userBot.loc = getRandomLoc();
+  autoBot.loc = getRandomLoc();
   refreshMap();
+}
+
+// sets the speed of the autonomous bot
+function setSpeed() {
+  let speed = document.getElementById("speed").value;
+  if (!isValidNumber(speed)) {
+    alert("Incorrect input. Please enter a positive integer.");
+    return;
+  }
+  waitCount = parseInt(speed);
 }
 
 // creates/removes wall on the clicked cell
@@ -87,8 +172,10 @@ function createWall(canvas, e) {
   i = Math.round((walledCellX/(canvasWidth/columns)) + columns*(walledCellY/(canvasHeight/rows)));
   console.log(walledCellX, walledCellY, i);
 
-  map[i].isWall = !map[i].isWall;
-  refreshMap();
+  if (!map[i].isOccupied) {
+    map[i].isWall = !map[i].isWall;
+    refreshMap();
+  }
 }
 
 // checks if the parameter is a valid positive integer
@@ -96,36 +183,55 @@ function isValidNumber(str) {
   return /^\s*\d+\s*$/.test(str);
 }
 
-// event listener - arrow keys
+// gets a raondom spawn location for the robot
+function getRandomLoc() {
+  let botIndex;
+  do {
+    botIndex = Math.floor(Math.random() * map.length);
+  } while(map[botIndex].isWall && map[botIndex].isOccupied);
+  return botIndex;
+}
+
+// document arrow keys event listener
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     e.preventDefault();
-    if (Math.floor(bot.x) != 1 && !map[map.indexOf(bot) - 1].isWall) {
-      bot = map[map.indexOf(bot) - 1];
+    if (Math.floor(map[userBot.loc].x) != 1 && !map[userBot.loc - 1].isWall && !map[userBot.loc - 1].isOccupied) {
+      map[userBot.loc].isOccupied = !map[userBot.loc].isOccupied;
+      userBot.loc--;
+      userBot.dir = 4;
       refreshMap();
     }
   } else if (e.which === 38) {
     e.preventDefault();
-    if (Math.floor(bot.y) != 1 && !map[map.indexOf(bot) - columns].isWall) {
-      bot = map[map.indexOf(bot) - columns];
+    if (Math.floor(map[userBot.loc].y) != 1 && !map[userBot.loc - columns].isWall && !map[userBot.loc - columns].isOccupied) {
+      map[userBot.loc].isOccupied = !map[userBot.loc].isOccupied;
+      userBot.loc -= columns;
+      userBot.dir = 1;
       refreshMap();
     }
   } else if (e.which === 39) {
     e.preventDefault();
-    if (Math.floor(bot.x) != Math.floor(1 + (columns - 1) * (canvasWidth / columns)) && !map[map.indexOf(bot) + 1].isWall) {
-      bot = map[map.indexOf(bot) + 1];
+    if (Math.floor(map[userBot.loc].x) != Math.floor(1 + (columns - 1) * (canvasWidth / columns)) && !map[userBot.loc + 1].isWall && !map[userBot.loc + 1].isOccupied) {
+      map[userBot.loc].isOccupied = !map[userBot.loc].isOccupied;
+      userBot.loc++;
+      userBot.dir = 2;
       refreshMap();
     }
   } else if (e.which === 40) {
     e.preventDefault();
-    if (Math.floor(bot.y) != Math.floor(1 + (rows - 1) * (canvasHeight / rows)) && !map[map.indexOf(bot) + columns].isWall) {
-      bot = map[map.indexOf(bot) + columns];
+    if (Math.floor(map[userBot.loc].y) != Math.floor(1 + (rows - 1) * (canvasHeight / rows)) && !map[userBot.loc + columns].isWall && !map[userBot.loc + columns].isOccupied) {
+      map[userBot.loc].isOccupied = !map[userBot.loc].isOccupied;
+      userBot.loc += columns;
+      userBot.dir = 3;
       refreshMap();
     }
   }
 });
 
-// event listener - onclick
+// canvas onclick event listener
 canvas.addEventListener("click", function(e) {
   createWall(canvas, e);
 });
+
+requestAnimationFrame(loop);
