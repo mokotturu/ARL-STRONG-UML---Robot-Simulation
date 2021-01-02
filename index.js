f404675ebd11310666e844bed672455d95c5f707
mokotturu <monishkotturu@gmail.com>
2021-01-01 22:01:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const path = require('path');
-
-const app = express();
-
-// Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+express()
+.use(express.static(path.join(__dirname, 'public')))
+.get('/', (req, res) => res.render(public/index))
+.listen(PORT, () => console.log(`Server started on port ${PORT}`));
