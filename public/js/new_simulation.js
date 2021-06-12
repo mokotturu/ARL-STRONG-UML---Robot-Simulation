c8c2ca4071e289a0b7cd0f5307ab49c4fccc25de
mokotturu <monishkotturu@gmail.com>
2021-06-12 01:33:33 -0400

    rebuilt game with classes and some optimizations (unfinished)

---
+++ public/js/new_simulation.js
@@ -0,0 +1,623 @@
+const $mapContainer = $('#map-container');
+const $map = $('#map');
+const $timer = $('#timer');
+const $popupModal = $('#popup-modal');
+const $minimapImage = $('#minimap');
+const $humanImage = $('#human-image');
+const $botImage = $('#bot-image');
+const $log = $('.tableItems');
+const $dropdown = $('#maps');
+const $progressbar = $('.background');
+const $agentText = $('.agent-text');
+$.jCanvas.defaults.fromCenter = false;
+
+var rows, columns, boxWidth, boxHeight;
+const canvasWidth = $map.width();
+const canvasHeight = $map.height();
+
+const colors = {
+	human: '#3333ff',
+	lightHuman: '#9999ff',
+	agent: '#ff3d5d',
+	lightAgent: '#ff9eae',
+	team: '#ffff7f',
+	lightTeam: '#ffff7f',
+	wall: 'black',
+	agent1: '#33ff70',
+	lightAgent1: '#99ffb7',
+	agent2: '#ff8000',
+	lightAgent2: '#ffbf7f',
+	victim: 'red',
+	hazard: 'yellow'
+};
+
+var grid;
+var uuid;
+var data = [{ movement: [], human: [], agent1: [], agent2: [] }, { movement: [], human: [], agent1: [], agent2: [] }];
+var obstacles = { victims: [], hazards: [] };
+// var victim0, victim1, victim2, victim3, victim4, victim5, victim6, victim7, victim8, victim9, hazard0, hazard1, hazard2, hazard3, hazard4, hazard5, hazard6, hazard7, hazard8, hazard9;
+var mapPaths = [
+	'src/data9.min.json',	//  0
+	'src/data9.min.json',	//  1
+	'src/data9.min.json',	//  2
+	'src/data9.min.json',	//  3
+	'src/data9.min.json',	//  4
+	'src/data9.min.json',	//  5
+	'src/data9.min.json',	//  6
+	'src/data9.min.json',	//  7
+	'src/data9.min.json',	//  8
+	'src/data9.min.json',	//  9
+	'src/data10.min.json',	// 10
+	'src/data11.min.json',	// 11
+	'src/data12.min.json',	// 12
+	'src/data13.min.json',	// 13
+	'src/data14.min.json'	// 14
+];
+var pathIndex = 10;
+var currentPath = mapPaths[pathIndex];
+
+var human, agent1;
+
+var count = 0, waitCount = 7, seconds = 0, timeout, startTime;
+var eventListenersAdded = false, fullMapDrawn = false, pause = false;
+var humanLeft, humanRight, humanTop, humanBottom, botLeft, botRight, botTop, botBottom;
+var intervalCount = 0, half = 0, intervals = 10, duration = 30;
+var log = { agent1: [], agent2: [] };
+
+var victimMarker = new Image();
+var hazardMarker = new Image();
+victimMarker.src = 'img/victim-marker-big.png';
+hazardMarker.src = 'img/hazard-marker-big.png';
+
+class Human {
+	constructor (x, y, dir, color, fovSize, explored, tempExplored) {
+		this.id = 'human';
+		this.x = x;
+		this.y = y;
+		this.dir = dir;
+		this.color = color;
+		this.fovSize = fovSize;
+		this.explored = explored;
+		this.tempExplored = tempExplored;
+	}
+
+	spawn(size) {
+		$map.drawRect({
+			fillStyle: this.color,
+			x: this.x * boxWidth, y: this.y * boxHeight,
+			width: (boxWidth - 1) * size, height: (boxHeight - 1) * size
+		});
+	}
+
+	drawFOVCells(fovSet) {
+		fovSet.forEach(cell => {
+			if (cell.isWall) {
+				$map.drawRect({
+					fillStyle: colors.wall,
+					strokeStyle: colors.human,
+					strokeWidth: 1,
+					cornerRadius: 2,
+					x: cell.x*boxWidth, y: cell.y*boxHeight,
+					width: boxWidth - 1, height: boxHeight - 1
+				});
+			} else {
+				$map.drawRect({
+					fillStyle: colors.lightHuman,
+					x: cell.x*boxWidth, y: cell.y*boxHeight,
+					width: boxWidth - 1, height: boxHeight - 1
+				});
+			}
+		})
+	}
+
+	moveLeft() {
+		if (this.x != 1 && !grid[this.x - 1][this.y].isWall) {
+			--this.x;
+			this.dir = 4;
+			updateScrollingPosition(this.x, this.y);
+		}
+	}
+
+	moveRight() {
+		if (this.x != columns - 1 && !grid[this.x + 1][this.y].isWall) {
+			++this.x;
+			this.dir = 2;
+			updateScrollingPosition(this.x, this.y);
+		}
+	}
+
+	moveUp() {
+		if (this.y != 1 && !grid[this.x][this.y - 1].isWall) {
+			--this.y;
+			this.dir = 1;
+			updateScrollingPosition(this.x, this.y);
+		}
+	}
+
+	moveDown() {
+		if (this.y != rows - 1 && !grid[this.x][this.y + 1].isWall) {
+			++this.y;
+			this.dir = 3;
+			updateScrollingPosition(this.x, this.y);
+		}
+	}
+}
+
+class Agent extends Human {
+	constructor (id, x, y, dir, color, fovSize, explored, tempExplored) {
+		super(x, y, dir, color, fovSize, explored, tempExplored);
+		this.id = id;
+		this.index = 0;
+	}
+
+	spawn(size) {
+		super.spawn(size);
+		$map.drawText({
+			fromCenter: true,
+			fillStyle: 'black',
+			x: this.x * boxWidth + boxWidth/2, y: this.y * boxHeight + boxHeight/2,
+			fontSize: boxWidth,
+			fontFamily: 'Montserrat, sans-serif',
+			text: this.id
+		});
+	}
+}
+
+class Obstacle {
+	constructor (x, y, color, isFound) {
+		this.x = x;
+		this.y = y;
+		this.color = color;
+		this.isFound = isFound;
+	}
+}
+
+// GAME BEGINS
+$(document).ready(async () => {
+	startTime = new Date();
+	uuid = sessionStorage.getItem('uuid');
+
+	$('.body-container').css('visibility', 'hidden');
+	$('.body-container').css('opacity', '0');
+	$('.loader').css('visibility', 'visible');
+	$('.loader').css('opacity', '1');
+
+	await initMap(currentPath);
+
+	human = new Human(262, 348, 1, colors.human, 8, new Set(), new Set());
+	agent1 = new Agent('1', 261, 347, 1, colors.agent1, 5, new Set(), new Set());
+
+	$('.loader').css('visibility', 'hidden');
+	$('.body-container').css('visibility', 'visible');
+	$('.body-container').css('opacity', '1');
+	
+	$(document).on('keydown', e => {
+		eventKeyHandlers(e);
+	});
+
+	requestAnimationFrame(loop);
+
+});
+
+// game loop
+function loop() {
+	refreshMap();
+	updateScrollingPosition(human.x, human.y);
+	requestAnimationFrame(loop);
+}
+
+// initialize the grid array with map data from json
+async function initMap(path) {
+	grid = [];
+	await $.getJSON(currentPath, data => {
+		rows = data.dimensions[0].rows;
+		columns = data.dimensions[0].columns;
+		boxWidth = Math.floor(canvasWidth/rows);
+		boxHeight = Math.floor(canvasHeight/columns);
+
+		for (let x = 0; x < columns; ++x) {
+			grid.push([]);
+			for (let y = 0; y < rows; ++y) {
+				grid[x].push({ x: x, y: y, isWall: data.map[x * columns + y].isWall == "true", isHumanExplored: false, isAgentExplored: false });
+			}
+		}
+	}).fail(() => {
+		alert('An error has occured while loading the map.');
+	});
+}
+
+function spawn(members, size) {
+	members.forEach(member => {
+		member.spawn(size);
+		/* if (member.id == 'victim' && member.isFound) {
+			$map.drawEllipse({
+				fromCenter: true,
+				fillStyle: member.color,
+				x: member.x * boxWidth + boxWidth/2, y: member.y * boxHeight + boxHeight/2,
+				width: (boxWidth - 1)*size, height: (boxHeight - 1)*size
+			});
+		} else if (member.id == 'hazard' && member.isFound) {
+			$map.drawPolygon({
+				fromCenter: true,
+				fillStyle: member.color,
+				x: member.x * boxWidth + boxWidth/2, y: member.y * boxHeight + boxHeight/2,
+				radius: ((boxWidth - 1)/2)*size,
+				sides: 3
+			});
+		} */
+	});
+}
+
+function refreshMap() {
+	// compute human FOV
+	let humanFOV = getFOV(human);
+	let humanFOVSet = new Set(humanFOV);
+
+	/* humanFOVSet.forEach(item => {
+		grid[item].isHumanExplored = true;
+		tempHumanExplored.add(item);
+
+		draw(grid[item], 0);
+
+		for (let i = 0; i < obstacles.length; ++i) {
+			if (item == obstacles[i].loc) {
+				obstacles[i].isFound = true;
+			}
+		}
+	}); */
+	human.drawFOVCells(humanFOVSet);
+
+	// compute agent FOV
+
+	// spawn players
+	spawn([human, agent1], 1);
+}
+
+// divides the square field of view around the human/agent into 4 distinct "quadrants"
+function getFOV(player) {
+	let thisSurroundings = [[], [], [], []];
+	let centerX = player.x, centerY = player.y;
+	let i = 0, j = 0;
+
+	// quadrant 1 - top right
+	for (let y = centerY; y >= centerY - player.fovSize; --y) {
+		for (let x = centerX; x <= centerX + player.fovSize; ++x) {
+			thisSurroundings[0].push({ tempX: i, tempY: j, realX: x, realY: y });
+			++i;
+		}
+		i = 0;
+		++j;
+	}
+
+	i = 0, j = 0;
+
+	// quadrant 2 - top left
+	for (let y = centerY; y >= centerY - player.fovSize; --y) {
+		for (let x = centerX; x >= centerX - player.fovSize; --x) {
+			thisSurroundings[1].push({ tempX: i, tempY: j, realX: x, realY: y });
+			++i;
+		}
+		i = 0;
+		++j;
+	}
+
+	i = 0, j = 0;
+
+	// quadrant 3 - bottom left
+	for (let y = centerY; y <= centerY + player.fovSize; ++y) {
+		for (let x = centerX; x >= centerX - player.fovSize; --x) {
+			thisSurroundings[2].push({ tempX: i, tempY: j, realX: x, realY: y });
+			++i;
+		}
+		i = 0;
+		++j;
+	}
+
+	i = 0, j = 0;
+
+	//quadrant 4 - bottom right
+	for (let y = centerY; y <= centerY + player.fovSize; ++y) {
+		for (let x = centerX; x <= centerX + player.fovSize; ++x) {
+			thisSurroundings[3].push({ tempX: i, tempY: j, realX: x, realY: y });
+			++i;
+		}
+		i = 0;
+		++j;
+	}
+
+	return castRays(thisSurroundings, player.fovSize);
+}
+
+// draw lines from the center (human/agent location) to the sqaures on the border of the FOV
+// and then calculates what cells are visible and what are blocked by walls
+function castRays(arr, viewRadius) {
+	let mySurroundings = [];
+
+	// quadrant 1 - top right
+	for (let i = viewRadius; i < arr[0].length; i += viewRadius + 1) {
+		mySurroundings.push(...bresenhams(arr[0][0], arr[0][i], 1, arr[0]));
+	}
+	for (let i = arr[0].length - viewRadius - 1; i < arr[0].length - 1; ++i) {
+		mySurroundings.push(...bresenhams(arr[0][0], arr[0][i], 1, arr[0]));
+	}
+
+	// quadrant 2 - top left
+	for (let i = viewRadius; i < arr[1].length; i += viewRadius + 1) {
+		mySurroundings.push(...bresenhams(arr[1][0], arr[1][i], 2, arr[1]));
+	}
+	for (let i = arr[1].length - viewRadius - 1; i < arr[1].length - 1; ++i) {
+		mySurroundings.push(...bresenhams(arr[1][0], arr[1][i], 2, arr[1]));
+	}
+
+	// quadrant 3 - bottom left
+	for (let i = viewRadius; i < arr[2].length; i += viewRadius + 1) {
+		mySurroundings.push(...bresenhams(arr[2][0], arr[2][i], 3, arr[2]));
+	}
+	for (let i = arr[2].length - viewRadius - 1; i < arr[2].length - 1; ++i) {
+		mySurroundings.push(...bresenhams(arr[2][0], arr[2][i], 3, arr[2]));
+	}
+
+	// quadrant 4 - bottom right
+	for (let i = viewRadius; i < arr[3].length; i += viewRadius + 1) {
+		mySurroundings.push(...bresenhams(arr[3][0], arr[3][i], 4, arr[3]));
+	}
+	for (let i = arr[3].length - viewRadius - 1; i < arr[3].length - 1; ++i) {
+		mySurroundings.push(...bresenhams(arr[3][0], arr[3][i], 4, arr[3]));
+	}
+
+	return mySurroundings;
+}
+
+// draws a line between two given cells
+// the line ends if there is a wall obstructing the straight line
+// this imitates casting a ray from cell1 to cell2
+function bresenhams(cell1, cell2, quad, thisGrid) {
+	switch (quad) {
+		case 1:
+			return bresenhamdsQuad1Helper(cell1, cell2);
+		case 2:
+			return bresenhamdsQuad2Helper(cell1, cell2);
+		case 3:
+			return bresenhamdsQuad3Helper(cell1, cell2);
+		case 4:
+			return bresenhamdsQuad4Helper(cell1, cell2);
+	}
+}
+
+function bresenhamdsQuad1Helper(cell1, cell2) {
+	let x1 = cell1.realX, y1 = cell1.realY, x2 = cell2.realX, y2 = cell2.realY;
+	let dx = x2 - x1, dy = y1 - y2;
+	let m = dy/dx;
+	let p;
+	let arr = [];
+	if (m >= 0 && m <= 1) {
+		p = (2*dy) - dx;
+		while (x1 < x2) {
+			if (p < 0) {
+				++x1;
+				p += 2*dy;
+				arr.push(grid[x1][y1]);
+				if (grid[x1][y1].isWall) break;
+			} else {
+				++x1;
+				--y1;
+				p += 2*(dy - dx);
+				arr.push(grid[x1][y1]);
+				if (grid[x1][y1].isWall) break;
+			}
+		}
+	} else if (m > 1) {
+		p = (2*dx) - dy;
+		while (y2 < y1) {
+			if (p < 0) {
+				--y1;
+				p += 2*dx;
+				arr.push(grid[x1][y1]);
+				if (grid[x1][y1].isWall) break;
+			} else {
+				++x1;
+				--y1;
+				p += 2*(dx - dy);
+				arr.push(grid[x1][y1]);
+				if (grid[x1][y1].isWall) break;
+			}
+		}
+	}
+
+	return arr;
+}
+
+function bresenhamdsQuad2Helper(cell1, cell2) {
+	let x1 = cell1.realX, y1 = cell1.realY, x2 = cell2.realX, y2 = cell2.realY;
+	let dx = x1 - x2, dy = y1 - y2;
+	let m = dy/dx;
+	let p;
+	let arr = [];
+	if (m >= 0 && m <= 1) {
+		p = (2*dy) - dx;
+		while (x2 < x1) {
+			if (p < 0) {
+				--x1;
+				p += 2*dy;
+				arr.push(grid[x1][y1]);
+				if (grid[x1][y1].isWall) break;
+			} else {
+				--x1;
+				--y1;
+				p += 2*(dy - dx);
+				arr.push(grid[x1][y1]);
+				if (grid[x1][y1].isWall) break;
+			}
+		}
+	} else if (m > 1) {
+		p = (2*dx) - dy;
+		while (y2 < y1) {
+			if (p < 0) {
+				--y1;
+				p += 2*dx;
+				arr.push(grid[x1][y1]);
+				if (grid[x1][y1].isWall) break;
+			} else {
+				--x1;
+				--y1;
+				p += 2*(dx - dy);
+				arr.push(grid[x1][y1]);
+				if (grid[x1][y1].isWall) break;
+			}
+		}
+	}
+
+	return arr;
+}
+
+function bresenhamdsQuad3Helper(cell1, cell2) {
+	let x1 = cell1.realX, y1 = cell1.realY, x2 = cell2.realX, y2 = cell2.realY;
+	let dx = x1 - x2, dy = y2 - y1;
+	let m = dy/dx;
+	let p;
+	let arr = [];
+	if (m >= 0 && m <= 1) {
+		p = (2*dy) - dx;
+		while (x2 < x1) {
+			if (p < 0) {
+				--x1;
+				p += 2*dy;
+				arr.push(grid[x1][y1]);
+				if (grid[x1][y1].isWall) break;
+			} else {
+				--x1;
+				++y1;
+				p += 2*(dy - dx);
+				arr.push(grid[x1][y1]);
+				if (grid[x1][y1].isWall) break;
+			}
+		}
+	} else if (m > 1) {
+		p = (2*dx) - dy;
+		while (y1 < y2) {
+			if (p < 0) {
+				++y1;
+				p += 2*dx;
+				arr.push(grid[x1][y1]);
+				if (grid[x1][y1].isWall) break;
+			} else {
+				--x1;
+				++y1;
+				p += 2*(dx - dy);
+				arr.push(grid[x1][y1]);
+				if (grid[x1][y1].isWall) break;
+			}
+		}
+	}
+
+	return arr;
+}
+
+function bresenhamdsQuad4Helper(cell1, cell2) {
+	let x1 = cell1.realX, y1 = cell1.realY, x2 = cell2.realX, y2 = cell2.realY;
+	let dx = x2 - x1, dy = y2 - y1;
+	let m = dy/dx;
+	let p;
+	let arr = [];
+	if (m >= 0 && m <= 1) {
+		p = (2*dy) - dx;
+		while (x1 < x2) {
+			if (p < 0) {
+				++x1;
+				p += 2*dy;
+				arr.push(grid[x1][y1]);
+				if (grid[x1][y1].isWall) break;
+			} else {
+				++x1;
+				++y1;
+				p += 2*(dy - dx);
+				arr.push(grid[x1][y1]);
+				if (grid[x1][y1].isWall) break;
+			}
+		}
+	} else if (m > 1) {
+		p = (2*dx) - dy;
+		while (y1 < y2) {
+			if (p < 0) {
+				++y1;
+				p += 2*dx;
+				arr.push(grid[x1][y1]);
+				if (grid[x1][y1].isWall) break;
+			} else {
+				++x1;
+				++y1;
+				p += 2*(dx - dy);
+				arr.push(grid[x1][y1]);
+				if (grid[x1][y1].isWall) break;
+			}
+		}
+	}
+
+	return arr;
+}
+
+// human controls
+function eventKeyHandlers(e) {
+	switch (e.keyCode) {
+		case 65:	// a
+		case 37:	// left arrow
+		case 72:	// h
+			e.preventDefault();
+			human.moveLeft();
+			break;
+		case 87:	// w
+		case 38:	// up arrow
+		case 75:	// k
+			e.preventDefault();
+			human.moveUp();
+			break;
+		case 68:	// d
+		case 39:	// right arrow
+		case 76:	// l
+			e.preventDefault();
+			human.moveRight();
+			break;
+		case 83:	// s
+		case 40:	// down arrow
+		case 74:	// j
+			e.preventDefault();
+			human.moveDown();
+			break;
+		case 49:	// 1
+			e.preventDefault();
+			// data[half].movement.push({ key: e.key, t: Math.round((performance.now()/1000) * 100)/100 });
+			updateScrollingPosition(grid[agent1.loc]);
+			break;
+		case 50:	// 2
+			e.preventDefault();
+			// data[half].movement.push({ key: e.key, t: Math.round((performance.now()/1000) * 100)/100 });
+			updateScrollingPosition(grid[agent2.loc]);
+		default:	// nothing
+			break;
+	}
+
+	let tracker = { loc: human.loc, t: Math.round((performance.now()/1000) * 100)/100 };
+	data[half].human.push(tracker);
+}
+
+function updateScrollingPosition(x, y) {
+	$mapContainer[0].scroll(x * boxWidth - $mapContainer.width()/2, y * boxHeight - $mapContainer.height()/2);
+}
+
+// SET METHODS
+
+function union(setA, setB) {
+	let _union = new Set(setA);
+	for (let elem of setB) {
+		_union.add(elem);
+	}
+	return _union;
+}
+
+function difference(setA, setB) {
+	let _difference = new Set(setA);
+	for (let elem of setB) {
+		_difference.delete(elem);
+	}
+	return _difference;
+}
